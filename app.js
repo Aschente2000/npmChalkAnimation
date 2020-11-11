c77ef1b408980fec0b26c0a59397a1a495bf6122
DANIELS <dd0924779@otc.edu>
2020-11-11 09:41:13 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+const chalkAnimation = require('chalk-animation');
+
+const log = console.log;
+
+
+log("once upon a time there was a man who was in"+chalk.red("Danger!")+" So the man ran away so fast that the wind behind him turned "+chalk.blue("blue.")+" Later that day the man decided that he would go for a walk in the "+chalkAnimation.rainbow('Magical Forest!')+" The forest turn out to be "+chalkAnimation.neon('Haunted!')+" And the man was never seen again");
